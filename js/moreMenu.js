2f907fa1e78f5d242ccf2c1df1628c1f96c86cae
James Yang <jamesyangf123@gmail.com>
2016-09-09 23:00:10 -0700

    Chang Home and Menu, Need to work on transition between pages and the logo and nav menu

--- js/moreMenu.js
+++ js/moreMenu.js
@@ -1,28 +1,29 @@
 var main = function() {
   /* Push the body and the nav over by 285px over */
   $('.more_menu').click( function() {
+    $('.bgimg-1').css('opacity', '0.5');
   	//move the menu screen
     $('.menu').animate({ //inside the animate method that takes two parameters
-      left: "0px" //changes its left attribute to zero
-    }, 200); //make it happen over 200 milliseconds
+      right: "0px" //changes its left attribute to zero
+    }, 300); //make it happen over 200 milliseconds
 
     //move the rest of the page 
     $('body').animate({
-      left: "285px"
-    }, 200);
+      left: "-355px"
+    }, 300);
   });
 
   /* Then push them back */
   $('.icon-close').click( function(){
-
+    $('.bgimg-1').css('opacity', '1');
     $('.menu').animate({
-      left: "-285px"
-    }, 200);
+      right: "-355px"
+    }, 300);
 
     //move the rest of the page 
     $('body').animate({
       left: "0px"
-    }, 200);
+    }, 300);
   });
 
 
--- menu/menu.php
+++ menu/menu.php
@@ -6,22 +6,65 @@
 		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
     	<link href='http://fonts.googleapis.com/css?family=Tangerine' rel='stylesheet' type='text/css'>
     	<link href='http://fonts.googleapis.com/css?family=Karla' rel='stylesheet' type='text/css'>
+    	<link href='http://fonts.googleapis.com/css?family=Yatra One' rel='stylesheet' type='text/css'>
     	<link href='../css/menu.css' rel='stylesheet'>
+    	<link href='../css/hover-min.css' rel='stylesheet'>
+    	<link href='../css/scrollAnimation.css' rel='stylesheet'>
+
+    	<!-- Include jQuery libraries from Google, loads jQuery-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<!-- The write our code for menu -->
+		<script src="../js/moreMenu.js"></script>
 	<head>
 
 	<body>
 
+		<!-- Beginning of Side Menu Nav-->
+		<div class="menu">
+
+			<!-- The Close Menu icon (The X)-->
+			<div class="icon-close">
+				<img src="../images/icon_close.png">
+			</div>
+
+			<!-- Menu -->
+			<ul>
+				<!-- a href is a link to something placeholer #-->
+				<li id="menu_home"><a href="../index.html">Home</a></li>
+				<li id="menu_menu"><a href="menu.php">Menu</a></li>
+				<li id="menu_location"><a href="#">Location</a></li>
+				<li id="menu_aboutus"><a href="#">About Us</a></li>
+			</ul>
+		</div> 
+		<!-- end of Side Menu Nav-->
 		<!-- This is the top part of the menu page -->
 		<div class="bgimg-1">
+
+			<!-- Beginning of Navigation -->
+			<div class="main_menu_containter" id="nav_container">
+				<div class="wbLogo"><img src="../images/logoP.png"></div>
+					<!-- The main menu list -->
+				<ul id="main_menu_list" class"main_menu_nav"> 
+					<li class="main_menu_home"><a href="../index.html"class="hvr-float-shadow">Home</a></li>
+					<li class="main_menu_menu"><a href="menu.php"class="hvr-float-shadow">Menu</a></li>
+					<li class="main_menu_location"><a href="#"class="hvr-float-shadow">Location</a></li>
+					<li class="main_menu_aboutus"><a href="#"class="hvr-float-shadow">About Us</a></li>		
+					<li class="more_menu"><img src="../images/menu-icon.png"class="hvr-float-shadow"></li>
+				</ul>
+			</div>
+			<!-- End of Navigation -->
+
+			
 			<div class="caption-1">
 				<span class="border">OUR MENU</span>
+				<div class="arrow bounce"></div>
 			</div>
 		</div>
 		<!-- End of top menu page -->
 
 		<!-- Beginning of Menu -->
 		<div class="menu-container">
-			<div class="kbbq-menu-title"> ALL YOU CAN EAT KBBQ</div>
+			<div class="kbbq-menu-title"> All You Can Eat KBBQ</div>
 			<p class="kbbq-lunch-menu-1">
 			<!-- php code that parses the menu text file -->
 			<?php 
@@ -34,6 +77,13 @@
 		</p>
 		</div>
 		<!-- End of menu -->
+
+		<!-- Beginning of footer -->
+		<hr></hr>
+		<div class="footer-container">
+			<div class="copyright">Copyright &copy; 2016 Warehouse Buffet. All rights reserved.</div>
+		</div>	
+		<!-- End of Footer -->
 
 	</body>
 
