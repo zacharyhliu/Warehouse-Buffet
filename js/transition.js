14cb10c1b8b3e19cb75a02ffcc1c67a365f088fb
jamesyangf <jamesyangf123@gmail.com>
2017-01-12 01:45:02 -0800

    Implement Page Transition and Added and updated pages
    Transitions from page to page. Created an about page. Made another main.css out of the css folder. Made a gallery for the location page

---
+++ js/transition.js
@@ -0,0 +1,5 @@
+// JQuery's new way of executing the functions
+$(function() {
+	//JQuery methods go here...
+	$('body').removeClass('fade-out');
+})
--- menu/menu.php
+++ menu/menu.php
@@ -9,17 +9,19 @@
     	<link href='http://fonts.googleapis.com/css?family=Karla' rel='stylesheet' type='text/css'>
     	<link href='http://fonts.googleapis.com/css?family=Yatra One' rel='stylesheet' type='text/css'>
     	<link href='http://fonts.googleapis.com/css?family=Dancing Script' rel='stylesheet' type='text/css'>
-    	<link href='../css/menu.css' rel='stylesheet'>
+    	<link href='menu.css' rel='stylesheet'>
     	<link href='../css/hover-min.css' rel='stylesheet'>
-    	<link href='../css/scrollAnimation.css' rel='stylesheet'>
+    	<link href='scrollAnimation.css' rel='stylesheet'>
+    	<link href='../css/transition.css' rel='stylesheet'>
 
     	<!-- Include jQuery libraries from Google, loads jQuery-->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<!-- The write our code for menu -->
 		<script src="../js/moreMenu.js"></script>
+		<script src="../js/transition.js"></script>
 	<head>
 
-	<body>
+	<body class="fade-out">
 		
 		<!-- Beginning of Main Navigation Bar -->
 		<div class="navigation-bar">
@@ -32,7 +34,7 @@
 					<li class="main_menu_home"><a href="../index.html"class="hvr-float-shadow">Home</a></li>
 					<li class="main_menu_menu"><a href="menu.php"class="hvr-float-shadow">Menu</a></li>
 					<li class="main_menu_location"><a href="../location/location.html"class="hvr-float-shadow">Location</a></li>
-					<li class="main_menu_aboutus"><a href="#"class="hvr-float-shadow">About Us</a></li>
+					<li class="main_menu_aboutus"><a href="../about/about.html"class="hvr-float-shadow">About Us</a></li>
 					<!--li class="more_menu"><img src="images/menu-icon.png"class="hvr-float-shadow"></li-->
 				</ul>
 				<div class="more_menu"><img src="../images/menu-icon.png"class="hvr-float-shadow"></div>
@@ -54,7 +56,7 @@
 				<li id="menu_home"><a href="../index.html"class="hvr-float-shadow">Home</a></li>
 				<li id="menu_menu"><a href="menu.php"class="hvr-float-shadow">Menu</a></li>
 				<li id="menu_location"><a href="../location/location.html"class="hvr-float-shadow">Location</a></li>
-				<li id="menu_aboutus"><a href="#"class="hvr-float-shadow">About Us</a></li>
+				<li id="menu_aboutus"><a href="../about/about.html"class="hvr-float-shadow">About Us</a></li>
 			</ul>
 		</div> 
 		<!-- end of Side Menu Nav-->
@@ -63,6 +65,8 @@
 		<div class="bgimg-1">
 			<div class="wb-menu-block">
 				<span class="border">Our Menu</span>
+				<br>
+				<span class="descript">Freshly Selected and Authentic</span>
 				<div class="arrow bounce"></div>
 			</div>
 		</div>
